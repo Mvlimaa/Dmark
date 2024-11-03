2c0ee4ceb0a5d553b2419485eab89d6780397e5a
Mvlima <marcelovynicius@gmail.com>
2024-11-03 16:30:57 -0300

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,19 @@ import Usuario from "./componets/Usuario";
 import Senha from "./componets/Senha";
 import Login from "./componets/Login";
 import Esqsenha from "./componets/Esqsenha";
+import styles from "../app/styles/LoginBox.module.css"
+import Cadastrar from "../app/componets/Cadastrar"
 export default function TelaLogin() {
   return (
     <>
     <Logo/>
+    <div className={styles.LoginBox}>
     <Usuario/>
     <Senha/>
     <Login/>
+    <Cadastrar/>
     <Esqsenha/>
+    </div>
     </>
   );
 }
